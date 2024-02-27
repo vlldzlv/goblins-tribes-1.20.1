41ffd4b45e6634b891a71072bab9d43e6ccab49d
vlldzlv <vladislove.exe@gmail.com>
2024-02-27 21:19:27 +0500

    Я добавил Рубин, Сырой Рубин и вкладку для них

--- src/main/java/net/vlldzlv/tutorialmod/TutorialMod.java
+++ src/main/java/net/vlldzlv/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.vlldzlv.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.vlldzlv.tutorialmod.item.ModItems;
+import net.vlldzlv.tutorialmod.item.ModItemsGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemsGroups.registerItemGroup();
+		ModItems.registerMOdItems();
 	}
 }
---
+++ src/main/java/net/vlldzlv/tutorialmod/item/ModItemsGroups.java
@@ -0,0 +1,27 @@
+package net.vlldzlv.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.vlldzlv.tutorialmod.TutorialMod;
+
+public class ModItemsGroups {
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TutorialMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+
+                    }).build());
+
+        public static void registerItemGroup(){
+            TutorialMod.LOGGER.info("Registering Item Group for " +TutorialMod.MOD_ID);
+        }
+}
